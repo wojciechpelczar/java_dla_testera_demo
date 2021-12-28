51a9f6d96d05aeceed967d1925e44df1ef776dcf
wojciechpelczar <pelczar.wojciech@gmail.com>
2021-12-28 13:21:20 +0100

    Changed "Zadanie domowe"

--- src/ZadanieDomowe1.java
+++ src/ZadanieDomowe1.java
@@ -1,5 +1,5 @@
 public class ZadanieDomowe1 {
     public static void main(String[] args) {
-        System.out.println("zadanie domowe 12");
+        System.out.println("zadanie domowe 123");
     }
 }
