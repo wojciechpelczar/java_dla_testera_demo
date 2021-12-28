af46a7dfd74c3350a53a665d03cc7a73fa306041
wojciechpelczar <pelczar.wojciech@gmail.com>
2021-12-28 15:21:19 +0100

    Added "TypyDanych"

---
+++ src/TypyDanych.java
@@ -0,0 +1,39 @@
+public class TypyDanych {
+
+    public static void main(String[] args) {
+        //START APLIKACJI
+
+        //liczby calkowite
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        int number = 10;
+        System.out.println(number);
+
+        //liczby zmiennoprzecinkowe
+        float f;
+        double d;
+
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        //tekst
+        String someText = "Przykładowy tekst";
+        System.out.println(someText);
+
+        //znak
+        char someChar = 'A';
+        System.out.println(someChar);
+
+        //prawda
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        //falsz
+        boolean falseValue = false;
+        System.out.println(falseValue);
+
+    }
+}
